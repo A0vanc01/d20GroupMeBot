335653a073c53699303467b34d79c6ee1ad56a98
AlexVance <AlexVance@hchb.com>
2018-02-12 12:44:29 -0500

    things

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import json
+import random
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    if data.text != "/roll":
+       return "ok", 200
+
+    log('Recieved {}'.format(data))
+
+    roll = random.randint(1, 20)
+    if roll == 1:
+        roll = "a failure"
+    elif roll < 5:
+        roll = roll + " ouch"
+    elif roll == 20:
+        roll = "CRITICAL"
+
+  # We don't want to reply to ourselves!
+    if data['name'] != 'vance-d20-bot':
+        msg = '{}, you rolled "{}".'.format(data['name'], roll)
+        send_message(msg)
+
+    return "ok", 200
+
+def send_message(msg):
+    #url  = 'https://api.groupme.com/v3/bots/post'
+    url = 'https://web.groupme.com/chats'
+
+    data = {
+        'bot_id' : os.getenv('GROUPME_BOT_ID'),
+        'text'   : msg,
+        }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+  
+def log(msg):
+    print(str(msg))
+    sys.stdout.flush()
